594ef1f1665458c8b0d941c31d9b4fb5b40197a5
sanyath007 <sanyath007@gmail.com>
2021-10-17 21:04:24 +0700

    Add loading spinner and pagination components to dashboard/_head partial view

--- resources/views/dashboard/_head.blade.php
+++ resources/views/dashboard/_head.blade.php
@@ -15,7 +15,7 @@
         </div>
     </div>
     <div class="box-body">
-        <table class="table table-triped">
+        <table class="table table-triped" style="margin-bottom: 1rem; ng-show="!loading">
             <tr>
                 <th style="width: 5%; text-align: center;">#</th>
                 <th>ชื่อ-สกุล</th>
@@ -23,11 +23,55 @@
                 <th style="width: 30%; text-align: center;">จำนวน (วัน)</th>
             </tr>
             <tr ng-repeat="(index, leave) in headLeaves">
-                <td style="text-align: center;">@{{ index+1 }}</td>
+                <td style="text-align: center;">@{{ pager.from+index }}</td>
                 <td>@{{ leave.person.person_firstname + ' ' + leave.person.person_lastname }}</td>
                 <td style="text-align: center;">@{{ leave.type.name }}</td>
                 <td style="text-align: center;">@{{ leave.leave_days }}</td>
             </tr>
         </table>
+
+        <ul class="pagination pagination-sm no-margin pull-right" ng-show="!loading">
+            <li ng-if="pager.current_page !== 1">
+                <a href="#" ng-click="getDebtWithURL(pager.first_page_url)" aria-label="Previous">
+                    <span aria-hidden="true">First</span>
+                </a>
+            </li>
+        
+            <li ng-class="{'disabled': (pager.current_page==1)}">
+                <a href="#" ng-click="getDebtWithURL(pager.prev_page_url)" aria-label="Prev">
+                    <span aria-hidden="true">Prev</span>
+                </a>
+            </li>
+
+            <li ng-repeat="i in debtPages" ng-class="{'active': pager.current_page==i}">
+                <a href="#" ng-click="getDebtWithURL(pager.path + '?page=' +i)">
+                    @{{ i }}
+                </a>
+            </li>
+
+            <!-- <li ng-if="pager.current_page < pager.last_page && (pager.last_page - pager.current_page) > 10">
+                <a href="#" ng-click="pager.path">
+                    ...
+                </a>
+            </li> -->
+
+            <li ng-class="{'disabled': (pager.current_page==pager.last_page)}">
+                <a href="#" ng-click="getDebtWithURL(pager.next_page_url)" aria-label="Next">
+                    <span aria-hidden="true">Next</span>
+                </a>
+            </li>
+
+            <li ng-if="pager.current_page !== pager.last_page">
+                <a href="#" ng-click="getDebtWithURL(pager.last_page_url)" aria-label="Previous">
+                    <span aria-hidden="true">Last</span>
+                </a>
+            </li>
+        </ul>
+        
+        <!-- Loading (remove the following to stop the loading)-->
+        <div ng-show="loading" class="overlay">
+            <i class="fa fa-refresh fa-spin"></i>
+        </div>
+        <!-- end loading -->
     </div>
 </div>
