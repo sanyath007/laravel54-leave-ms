2d2f9bacc0062b0bb62b577ae4f0dfd7e940e19b
sanyath007 <sanyath007@gmail.com>
2021-10-17 20:51:53 +0700

    Split small-box component from home view to dashboard/_stat-cards partial view

---
+++ resources/views/dashboard/_stat-cards.blade.php
@@ -0,0 +1,53 @@
+<div class="col-lg-3 col-xs-6">
+    <div class="small-box bg-aqua">
+        <div class="inner">
+            <h3>0</h3>
+
+            <p><h4>ลาป่วยทั้งหมด</h4></p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-bag"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+</div><!-- ./col -->
+
+<div class="col-lg-3 col-xs-6">
+    <div class="small-box bg-green">
+        <div class="inner">
+            <h3>0</h3><!-- <sup style="font-size: 20px">%</sup> -->
+
+            <p><h4>ลากิจทั้งหมด</h4></p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+</div><!-- ./col -->
+<div class="col-lg-3 col-xs-6">
+    <div class="small-box bg-yellow">
+        <div class="inner">
+            <h3>0</h3>
+
+            <p><h4>ลาพักผ่อนทั้งหมด</h4></p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-person-add"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+</div><!-- ./col -->
+<div class="col-lg-3 col-xs-6">
+    <div class="small-box bg-red">
+        <div class="inner">
+            <h3>0</h3>
+
+            <p><h4>ลาคลอดทั้งหมด</h4></p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-pie-graph"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+</div><!-- ./col -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,65 +19,7 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
 
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-aqua">
-                    <div class="inner">
-                        <h3>0</h3>
-
-                        <p><h4>ลาป่วยทั้งหมด</h4></p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div><!-- ./col -->
-
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-green">
-                    <div class="inner">
-                        <h3>0</h3><!-- <sup style="font-size: 20px">%</sup> -->
-
-                        <p><h4>ลากิจทั้งหมด</h4></p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div><!-- ./col -->
-
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-yellow">
-                    <div class="inner">
-                        <h3>0</h3>
-
-                        <p><h4>ลาพักผ่อนทั้งหมด</h4></p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div><!-- ./col -->
-
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-red">
-                    <div class="inner">
-                        <h3>0</h3>
-
-                        <p><h4>ลาคลอดทั้งหมด</h4></p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div><!-- ./col -->
+            @include('dashboard._stat-cards')
 
         </div><!-- /.row -->
 
@@ -97,7 +39,7 @@
             </section> -->
 
             <section class="col-lg-8 connectedSortable">
-                <!-- // หัวหน้า -->
+
                 @include('dashboard._head')
 
             </section>
