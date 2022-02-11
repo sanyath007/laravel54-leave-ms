fe731b4bfeea5d3538455db1d8eebbeff141cf0c
sanyath007 <sanyath007@gmail.com>
2022-02-11 13:43:55 +0700

    Delete unused methods in reportCtrl

--- public/js/controllers/reportCtrl.js
+++ public/js/controllers/reportCtrl.js
@@ -46,78 +46,44 @@ app.controller(
                             : moment().year() + 543 
                         : $scope.dtpYear;
 
-            $http
-                .get(`${CONFIG.baseUrl}/reports/summary-data?depart=${depart}&division=${division}&year=${year}`)
-                .then(function (res) {
-                    const { leaves, histories, persons } = res.data;
-                    const { data, ...pager } = persons;
+            $http.get(`${CONFIG.baseUrl}/reports/summary-data?depart=${depart}&division=${division}&year=${year}`)
+            .then(function (res) {
+                const { leaves, histories, persons } = res.data;
+                const { data, ...pager } = persons;
 
-                    $scope.data = data;
-                    $scope.pager = pager;
+                $scope.data = data;
+                $scope.pager = pager;
 
-                    /** Set each history's days instead of leave_days value */
-                    leaves.map(leave => {
-                        const leaveHistory = histories.find(history => history.person_id === leave.leave_person);
+                /** Set each history's days instead of leave_days value */
+                leaves.map(leave => {
+                    const leaveHistory = histories.find(history => history.person_id === leave.leave_person);
 
-                        leave['ill_days'] = leaveHistory['ill_days'];
-                        leave['per_days'] = leaveHistory['per_days'];
-                        leave['vac_days'] = leaveHistory['vac_days'];
-                        leave['lab_days'] = leaveHistory['lab_days'];
-                        leave['hel_days'] = leaveHistory['hel_days'];
-                        leave['ord_days'] = leaveHistory['ord_days'];
+                    leave['ill_days'] = leaveHistory['ill_days'];
+                    leave['per_days'] = leaveHistory['per_days'];
+                    leave['vac_days'] = leaveHistory['vac_days'];
+                    leave['lab_days'] = leaveHistory['lab_days'];
+                    leave['hel_days'] = leaveHistory['hel_days'];
+                    leave['ord_days'] = leaveHistory['ord_days'];
 
-                        return leave;
-                    });
+                    return leave;
+                });
 
-                    /** Append leave data to each person */
-                    $scope.data = data.map(person => {
-                        const leave = leaves.find((leave) =>
-                            person.person_id === leave.leave_person
-                        );
-                        return {
-                            ...person,
-                            leave: leave,
-                        };
-                    });
+                /** Append leave data to each person */
+                $scope.data = data.map(person => {
+                    const leave = leaves.find((leave) =>
+                        person.person_id === leave.leave_person
+                    );
+                    return {
+                        ...person,
+                        leave: leave,
+                    };
+                });
 
-                    $scope.loading = false;
-                }, function (err) {
-                    console.log(err);
-                    $scope.loading = false;
-                }
-            );
-        };
-
-        $scope.getDebtData = function (URL) {
-            $scope.debts = [];
-            $scope.pager = [];
-            $scope.loading = true;
-
-            var debtDate = $("#debtDate").val().split(",");
-            var sDate = debtDate[0].trim();
-            var eDate = debtDate[1].trim();
-            var debtType =
-                $("#debtType").val() != "" ? $("#debtType").val() : 0;
-            var showAll = $("#showall:checked").val() == "on" ? 1 : 0;
-
-            $http
-                .get(`${CONFIG.baseUrl}${URL}/${debtType}/${sDate}/${eDate}/${showAll}`)
-                .then(function (res) {
-                        console.log(res);
-                        $scope.debts = res.data.pager.data;
-                        $scope.pager = res.data.pager;
-
-                        $scope.pages = PaginateService.createPagerNo(
-                            $scope.pager
-                        );
-
-                        console.log($scope.pages);
-                        $scope.loading = false;
-                    }, function (err) {
-                        console.log(err);
-                        $scope.loading = false;
-                    }
-                );
+                $scope.loading = false;
+            }, function (err) {
+                console.log(err);
+                $scope.loading = false;
+            });
         };
 
         $scope.getDataWithURL = function (URL) {
@@ -132,8 +98,8 @@ app.controller(
                             : moment().year() + 543 
                         : $scope.dtpYear;
 
-            $http.get(`${URL}&depart=${depart}&year=${year}`).then(
-                function (res) {
+            $http.get(`${URL}&depart=${depart}&division=${division}&year=${year}`)
+            .then(function (res) {
                     console.log(res);
                     const { data, ...pager } = res.data.persons;
                     $scope.data = data;
@@ -150,29 +116,10 @@ app.controller(
                     });
 
                     $scope.loading = false;
-                },
-                function (err) {
+            }, function (err) {
                     console.log(err);
                     $scope.loading = false;
-                }
-            );
-        };
-
-        $scope.debtCreditorToExcel = function (URL) {
-            console.log($scope.debts);
-
-            if ($scope.debts.length == 0) {
-                toaster.pop("warning", "", "ไม่พบข้อมูล !!!");
-            } else {
-                var debtDate = $("#debtDate").val().split(",");
-                var sDate = debtDate[0].trim();
-                var eDate = debtDate[1].trim();
-                var creditor =
-                    $("#debtType").val() == "" ? "0" : $("#debtType").val();
-                var showAll = $("#showall:checked").val() == "on" ? 1 : 0;
-
-                window.location.href = `${CONFIG.baseUrl}${URL}/${creditor}/${sDate}/${eDate}/${showAll}`;
-            }
+            });
         };
 
         $scope.debttypeToExcel = function (URL) {
