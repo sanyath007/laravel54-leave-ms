757bd1316ed4ff59e1c4d6dc1ea3a217ace92107
sanyath007 <sanyath007@gmail.com>
2022-02-28 09:58:33 +0700

    Modify getDataWithURL methods of historiesCtrl by adding 1st arguments as event type of js and check if its currentTarget's parent element have .disabled class just return and to do nothing and finally edit ng-click of pagination's link button by adding $event as 1st parameter

--- public/js/controllers/historyCtrl.js
+++ public/js/controllers/historyCtrl.js
@@ -3,15 +3,14 @@ app.controller('historyCtrl', function(CONFIG, $scope, $http, toaster, ModalServ
     $scope.leaves = [];
     $scope.pager = [];
     $scope.loading = false;
-
     $scope.cboLeaveType = '';
     $scope.cboYear = parseInt(moment().format('MM')) > 9
                         ? (moment().year() + 544).toString()
                         : (moment().year() + 543).toString();
     $scope.budgetYearRange = [2560,2561,2562,2563,2564,2565,2566,2567];
-
     $scope.histories = null;
     $scope.vacation = null;
+
     $scope.getSummary = function(personId) {
         $scope.loading = true;
 
@@ -34,33 +33,38 @@ app.controller('historyCtrl', function(CONFIG, $scope, $http, toaster, ModalServ
 
         $http.get(`${CONFIG.baseUrl}/histories/${personId}/${$scope.cboYear}/person?type=${type}`)
         .then(function(res) {
-            const { data, ...pager } = res.data.leaves;
-
-            $scope.leaves = data;
-            $scope.pager = pager;
-
             $scope.loading = false;
+
+            $scope.setLeaves(res);
         }, function(err) {
             console.log(err);
             $scope.loading = false;
         });
-    }
+    };
 
-    $scope.getDataWithURL = function(URL) {
+    $scope.setLeaves = function(res) {
+        const { data, ...pager } = res.data.leaves;
+
+        $scope.leaves = data;
+        $scope.pager = pager;
+    };
+
+    $scope.getDataWithURL = function(e, URL, cb) {
+		/** Check whether parent of clicked a tag is .disabled just do nothing */
+		if ($(e.currentTarget).parent().is('li.disabled')) return;
+
         $scope.leaves = [];
         $scope.pager = [];
+
         $scope.loading = true;
-        
+
         const type = $scope.cboLeaveType === '' ? '' : $scope.cboLeaveType;
+
         console.log(`${URL}&type=${type}`);
 
         $http.get(`${URL}&type=${type}`)
         .then(function(res) {
-            console.log(res);
-            const { data, ...pager } = res.data.leaves;
-
-            $scope.leaves = data;
-            $scope.pager = pager;
+            $scope.setLeaves(res);
 
             $scope.loading = false;
         }, function(err) {
