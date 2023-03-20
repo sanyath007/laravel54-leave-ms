5af9951308a0800253da322c2efda942e646a181
sanyath007 <sanyath007@gmail.com>
2023-03-20 16:29:55 +0700

    Modify search method of LeaveController by setting depart and division vars according to logged in user's duty

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -153,8 +153,12 @@ class LeaveController extends Controller
 
         /** Get params from query string */
         $faction  = Auth::user()->person_id == '1300200009261' ? '' : Auth::user()->memberOf->faction_id;
-        $depart   = Auth::user()->person_id == '1300200009261' ? '' : Auth::user()->memberOf->depart_id;
-        $division = Auth::user()->person_id == '1300200009261' ? '' : Auth::user()->memberOf->ward_id;
+        $depart   = Auth::user()->person_id == '1300200009261'
+                        ? ''
+                        : (Auth::user()->memberOf->duty_id == 1 ? '' : Auth::user()->memberOf->depart_id);
+        $division = Auth::user()->person_id == '1300200009261'
+                        ? ''
+                        : (Auth::user()->memberOf->duty_id == 2 ? '' : Auth::user()->memberOf->ward_id);
         $name     = $req->get('name');
         $month    = $req->get('month');
 
